97e49be0c6a953e76a617d0a3dc5a81ad57dd419
HENRYMARTIN5 <62612165+HENRYMARTIN5@users.noreply.github.com>
2022-05-12 08:43:10 -0500

    checkpoint fixed?

--- Clarity.js
+++ Clarity.js
@@ -151,13 +151,16 @@ Clarity.prototype.load_map = function (map) {
     });
   });
 
-  if (!spawnfound) {
-    this.current_map.player.x = 1
-    this.current_map.player.y = 1
-  } else {
-    this.current_map.player.x = spawnx;
-    this.current_map.player.y = spawny;
+  if (!this.checkpoint){
+    if (!spawnfound) {
+      this.current_map.player.x = 1
+      this.current_map.player.y = 1
+    } else {
+      this.current_map.player.x = spawnx;
+      this.current_map.player.y = spawny;
+    }
   }
+
 
   this.current_map.width_p = this.current_map.width * this.tile_size;
   this.current_map.height_p = this.current_map.height * this.tile_size;
