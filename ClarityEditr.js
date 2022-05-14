bb9e2c3d35f8aeda4ad359016c81d29a831e16a6
HENRYMARTIN5 <62612165+HENRYMARTIN5@users.noreply.github.com>
2022-05-14 07:57:28 -0500

    Editr Fixes

--- ClarityEditr.js
+++ ClarityEditr.js
@@ -1,4 +1,4 @@
-const container = document.querySelector('.container')
+const container = document.querySelector('#container');
 const sizeEl = document.querySelector('.size')
 let size = sizeEl.value
 const resetBtn = document.querySelector('.btn')
