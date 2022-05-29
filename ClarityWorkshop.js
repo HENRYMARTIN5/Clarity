778ee08013606a0c44c5e7c4d7c39aa4572f7056
Restyled.io <commits@restyled.io>
2022-05-29 21:38:53 +0000

    Restyled by prettier

--- ClarityWorkshop.js
+++ ClarityWorkshop.js
@@ -1,32 +1,32 @@
 function splitIntoChunk(inputArray, perChunk) {
   var result = inputArray.reduce((resultArray, item, index) => {
-    const chunkIndex = Math.floor(index / perChunk)
+    const chunkIndex = Math.floor(index / perChunk);
 
     if (!resultArray[chunkIndex]) {
-      resultArray[chunkIndex] = [] // start a new chunk
+      resultArray[chunkIndex] = []; // start a new chunk
     }
 
-    resultArray[chunkIndex].push(item)
+    resultArray[chunkIndex].push(item);
 
-    return resultArray
-  }, [])
-  return result
+    return resultArray;
+  }, []);
+  return result;
 }
 
-window.onload =
-    function() {
+window.onload = function () {
   var request = new XMLHttpRequest();
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  if (urlParams.has('page')) {
-    var page = urlParams.get('page');
+  if (urlParams.has("page")) {
+    var page = urlParams.get("page");
   } else {
     var page = 1;
   }
-  request.open('GET',
-               'https://clarityworkshop.n3rdl0rd.repl.co/getpage/' +
-                   page.toString(),
-               false); // `false` makes the request synchronous
+  request.open(
+    "GET",
+    "https://clarityworkshop.n3rdl0rd.repl.co/getpage/" + page.toString(),
+    false
+  ); // `false` makes the request synchronous
   request.send(null);
   if (request.status === 200) {
     var currentLvls = request.responseText;
@@ -62,7 +62,7 @@ window.onload =
     <br></br>
       </div>
       <p>Other Levels</p>
-    </div>`
+    </div>`;
 
     //   <div class="w3-third w3-container w3-margin-bottom">
     //   <div class="w3-container level w3-hover-opacity" style= "cursor:
@@ -72,13 +72,13 @@ window.onload =
     //   </div>
     // </div>
 
-    splitLvlJson.forEach(levels => {
+    splitLvlJson.forEach((levels) => {
       console.log(levels);
       const container = document.getElementById("container");
       var newRow = document.createElement("div");
       newRow.class = "w3-row-padding";
 
-      levels.forEach(level => {
+      levels.forEach((level) => {
         console.log(level);
         var newelem = `
     <div class="w3-third w3-container w3-margin-bottom w3-hover-opacity" style= "cursor: pointer;" onclick="loadLevelById({levelId});">
@@ -86,38 +86,43 @@ window.onload =
         <p><b>{level}</b></p>
         <p>By {author}</p>
       </div>
-    </div>`.replace("{level}", level["name"])
-                          .replace("{author}", level["author"])
-                          .replace("{levelId}", level["id"])
+    </div>`
+          .replace("{level}", level["name"])
+          .replace("{author}", level["author"])
+          .replace("{levelId}", level["id"]);
         newRow.innerHTML = newRow.innerHTML + newelem;
-      })
+      });
       newRow.innerHTML = newRow.innerHTML + "</br>";
       container.appendChild(newRow);
-    })
+    });
     // append pagination buttons (back, fwd, etc)
-    container.innerHTML = container.innerHTML + `
+    container.innerHTML =
+      container.innerHTML +
+      `
     <center>
     <div id="pagination" class="w3-row-padding">
       <div class="w3-button w3-black" style= "cursor: pointer;" onclick="loadPage(` +
-                          (parseInt(page) - 1).toString() + `);">
+      (parseInt(page) - 1).toString() +
+      `);">
         <p>Back</p>
       </div>
       <div class="w3-button w3-black" style= "cursor: pointer;" onclick="loadPage(` +
-                          (parseInt(page) + 1).toString() + `);">
+      (parseInt(page) + 1).toString() +
+      `);">
         <p>Forward</p>
       </div>
     </div>
-    </center>`
+    </center>`;
   }
-}
+};
 
 function loadPage(page) {
   window.location.href =
-      "https://henrymartin5.github.io/Clarity/workshop.html?page=" +
-      (page).toString();
+    "https://henrymartin5.github.io/Clarity/workshop.html?page=" +
+    page.toString();
 }
 
 function loadLevelById(id) {
   window.location.href =
-      "https://henrymartin5.github.io/Clarity/level.html?id=" + (id).toString();
+    "https://henrymartin5.github.io/Clarity/level.html?id=" + id.toString();
 }
